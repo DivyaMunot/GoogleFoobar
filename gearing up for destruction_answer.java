b9d3d0db8f58efa55fbc2ab149646ace79b18e94
Divya Munot <41624029+DivyaMunot@users.noreply.github.com>
2020-05-13 16:53:23 +0530

    Add files via upload

---
+++ gearing up for destruction_answer.java
@@ -0,0 +1,31 @@
+import java. util.*;
+import java.io.*;
+
+public class g{
+	public static int[] answer(int[] pegs){
+		int a = pegs[0];
+		int temp = -1;
+
+		for(int peg: pegs){
+			a = a + (2*peg*temp);
+			temp = temp * -1;
+		}
+
+		a = a + pegs[pegs.length-1] * temp;
+		a = a * 2;
+		int b;
+		if (pegs.length%2 == 0)
+			b = 3;
+		else
+			b = 1;
+
+		float ratio = (float)a / (float)b;
+		for(int i=0; i<pegs.length-2; i++){
+			int gear = pegs[i+1] - pegs[i];
+			if(ratio < 0 || ratio > (gear-1)) return new int[] {-1, -1};
+			ratio = gear - ratio;
+		}
+
+		return new int[] {a, b};
+	}
+}
